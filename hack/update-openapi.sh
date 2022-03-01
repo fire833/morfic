4d3a552a14b42cb8453264351d7d4453f8a8eb07
Kendall Tauser <kttpsy@gmail.com>
2022-03-01 14:37:08 -0600

    dns: worked on api spec, worked on hack scripts

--- hack/update-openapi.sh
+++ hack/update-openapi.sh
@@ -1,2 +1,28 @@
 #!/bin/bash
 
+echo "Rebuilding addresses API..."
+openapi-gen --input-dirs ../pkg/apis/addresses -h boilerplate.go.txt -O zz_generated.openapi &
+echo "Rebuilding authentication API..."
+openapi-gen --input-dirs ../pkg/apis/authentication -h boilerplate.go.txt -O zz_generated.openapi &
+echo "Rebuilding config API..."
+openapi-gen --input-dirs ../pkg/apis/config -h boilerplate.go.txt -O zz_generated.openapi &
+echo "Rebuilding dns API..."
+openapi-gen --input-dirs ../pkg/apis/dns -h boilerplate.go.txt -O zz_generated.openapi &
+echo "Rebuilding firewall API..."
+openapi-gen --input-dirs ../pkg/apis/firewall -h boilerplate.go.txt -O zz_generated.openapi &
+echo "Rebuilding interfaces API..."
+openapi-gen --input-dirs ../pkg/apis/interfaces -h boilerplate.go.txt -O zz_generated.openapi &
+echo "Rebuilding metrics API..."
+openapi-gen --input-dirs ../pkg/apis/metrics -h boilerplate.go.txt -O zz_generated.openapi &
+echo "Rebuilding nat API..."
+openapi-gen --input-dirs ../pkg/apis/nat -h boilerplate.go.txt -O zz_generated.openapi &
+echo "Rebuilding neighbors API..."
+openapi-gen --input-dirs ../pkg/apis/neighbors -h boilerplate.go.txt -O zz_generated.openapi &
+echo "Rebuilding routes API..."
+openapi-gen --input-dirs ../pkg/apis/routes -h boilerplate.go.txt -O zz_generated.openapi &
+echo "Rebuilding services API..."
+openapi-gen --input-dirs ../pkg/apis/services -h boilerplate.go.txt -O zz_generated.openapi &
+echo "Rebuilding vpn API..."
+openapi-gen --input-dirs ../pkg/apis/vpn -h boilerplate.go.txt -O zz_generated.openapi &
+
+wait
--- pkg/apis/dns/types.go
+++ pkg/apis/dns/types.go
@@ -22,6 +22,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+// RecordType defines the type of a DNS record.
 type RecordType int
 
 const (
@@ -31,6 +32,16 @@ const (
 	TXTRecord
 	SRVRecord
 	MXRecord
+)
+
+// ProviderStatus defines the status of a provider from the perspective of the control plane.
+type ProviderStatus int
+
+// Available statuses for provider.
+const (
+	Available ProviderStatus = iota
+	PluginError
+	Unavailable
 )
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
@@ -68,10 +79,21 @@ type DNSRecord struct {
 
 // DNSRecordSpec specifies the desired specification for a DNS record.
 type DNSRecordSpec struct {
-	Type  string `json:"type" yaml:"type"`
-	Host  string `json:"host" yaml:"host"`
+	// Type specifies the type of this DNS entry. The supported values
+	// are notated in the above enum for types.
+	Type RecordType `json:"type" yaml:"type"`
+
+	// Specify the hostname that is to be controlled via this record.
+	// Should be formatted as <subdomain_to_be_used>.<domain>.<tld>.
+	Host string `json:"host" yaml:"host"`
+
+	// The value for the record.
 	Value string `json:"value" yaml:"value"`
-	TTL   uint   `json:"ttl" yaml:"ttl"`
+
+	// Time to live for the record, cannot be negative.
+	//
+	// +optional
+	TTL uint `json:"ttl" yaml:"ttl"`
 }
 
 // DNSRecordStatus describes the current status of a DNS record.
@@ -119,8 +141,57 @@ type DNSProvider struct {
 
 // DNSProviderSpec specifies the desired spec for a DNS provider.
 type DNSProviderSpec struct {
+	// Specify the name for this DNS provider for reference by other
+	// resources and/or the control plane itself.
+	Name string `json:"name" yaml:"name"`
+
+	// Specify a cloudflare provider as the backend for this provider object.
+	//
+	// +optional
+	Cloudflare CloudflareProviderSpec `json:"cloudflare" yaml:"cloudflare"`
+
+	// Specify a route53 provider as the backend for this provider object.
+	//
+	// +optional
+	Route53 Route53ProviderSpec `json:"route53" yaml:"route53"`
+}
+
+// CloudflareProviderSpec represents the specifications required for using
+// cloudflare to update/maintain DNS records.
+type CloudflareProviderSpec struct {
+	// Specify the secret name to pull credentials from for this backend.
+	//
+	// This secrets should contain a key cloudflare.api.id and cloudflare.api.key,
+	// corresponding to theapi key id and key secret id that are needed to authenticate
+	// with the cloudflare API.
+	//
+	// cloudflare.api.zone is another key that can be used to store the zone id for cloudflare
+	// instead of specifying the ZoneId field within this spec object. The ZoneId field will take
+	// precedence over any keys that are found in secretName, however.
+	SecretName string `json:"secretName" yaml:"secretName"`
+
+	// Specify the zone Id that this provider will manage with the provided API. This field will take
+	// priority over an id that was specified in the secret secretName references.
+	//
+	// +optional
+	ZoneId string `json:"zone" yaml:"zone"`
+}
+
+// Route53ProviderSpec represents the specifications required for using
+// Route 53 from AWS to update/maintain DNS records.
+type Route53ProviderSpec struct {
 }
 
 // DNSProviderStatus represents the current status for a DNS provider.
 type DNSProviderStatus struct {
+	// Enabled defines if the provider is enabled and has been verified as working by the control plane.
+	Enabled bool `json:"enabled" yaml:"enabled"`
+
+	// Status Defines what the current status of the provider.
+	Status ProviderStatus `json:"status" yaml:"status"`
+
+	// StatusString represents a more verbose string status of the provider.
+	//
+	// +optional
+	StatusString string `json:"statusString" yaml:"statusString"`
 }
