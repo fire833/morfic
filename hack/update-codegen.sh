a8d9b8a7219ccaac2b5d7aea92f05248e8e24519
Kendall Tauser <kttpsy@gmail.com>
2022-03-11 23:41:00 -0600

    worked on api generation

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-export GOPATH=$(go env GOPATH)
+# export GOPATH=$(go env GOPATH)
 
-./update-deepcopy.sh
-./update-clients.sh
-./update-informers.sh
-./update-listers.sh
+GOPATH=$(go env GOPATH) ./generate-internal-groups.sh \
+deepcopy,client,lister,informer,openapi \
+github.com/fire833/vroute/pkg/client github.com/fire833/vroute/pkg/apis github.com/fire833/vroute/pkg/apis \
+"addresses.vroute.io:v1alpha1 authentication.vroute.io:v1 certificates.vroute.io:v1alpha1 config.vroute.io:v1 dns.vroute.io:v1alpha1 firewall.vroute.io:v1alpha1 interfaces.vroute.io:v1alpha1 metrics.vroute.io:v1alpha1 nat.vroute.io:v1alpha1 neighbors.vroute.io:v1alpha1 routes.vroute.io:v1 services.vroute.io:v1alpha1 vpn.vroute.io:v1alpha1" \
+--go-header-file boilerplate.go.txt \
 
 # wait
