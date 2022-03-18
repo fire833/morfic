7e2bcc366f2c8356975e0a0d9735cb0d0182cd6e
Kendall Tauser <kttpsy@gmail.com>
2022-03-17 23:45:37 -0500

    ugghhhhhhhh

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -3,10 +3,16 @@
 # export GOPATH=$(go env GOPATH)
 
 GOPATH=$(go env GOPATH) ./generate-internal-groups.sh \
-deepcopy,client,lister,informer \
+client,lister,informer \
 github.com/fire833/vroute/pkg/client github.com/fire833/vroute/pkg/apis github.com/fire833/vroute/pkg/apis \
-"addresses.vroute.io:v1alpha1 authentication.vroute.io:v1 certificates.vroute.io:v1alpha1 config.vroute.io:v1 dns.vroute.io:v1alpha1 firewall.vroute.io:v1alpha1 interfaces.vroute.io:v1alpha1 metrics.vroute.io:v1alpha1 nat.vroute.io:v1alpha1 neighbors.vroute.io:v1alpha1 routes.vroute.io:v1 services.vroute.io:v1alpha1 vpn.vroute.io:v1alpha1" \
+"addresses:v1alpha1 authentication:v1 certificates:v1alpha1 config:v1 dns:v1alpha1 firewall:v1alpha1 interfaces:v1alpha1 metrics:v1alpha1 nat:v1alpha1 neighbors:v1alpha1 routes:v1 services:v1alpha1 vpn:v1alpha1" \
 --go-header-file boilerplate.go.txt \
 # --output-base ../pkg/client
+
+# GOPATH=$(go env GOPATH) ./generate-groups.sh \
+# all \
+# github.com/fire833/vroute/pkg/client github.com/fire833/vroute/pkg/apis \
+# "addresses:v1alpha1 authentication:v1 certificates:v1alpha1 config:v1 dns:v1alpha1 firewall:v1alpha1 interfaces:v1alpha1 metrics:v1alpha1 nat:v1alpha1 neighbors:v1alpha1 routes:v1 services:v1alpha1 vpn:v1alpha1" \
+# --go-header-file boilerplate.go.txt \
 
 # wait
